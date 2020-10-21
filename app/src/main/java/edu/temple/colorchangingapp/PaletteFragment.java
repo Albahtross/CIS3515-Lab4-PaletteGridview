4fb218442dd280d3a67017f8eadb85b0ee5b3046
Albahtross <alexliu401@gmail.com>
2020-10-21 16:34:35 -0400

    working on palette frag, add extends

--- app/src/main/java/edu/temple/colorchangingapp/PaletteFragment.java
+++ app/src/main/java/edu/temple/colorchangingapp/PaletteFragment.java
@@ -1,7 +1,16 @@
 package edu.temple.colorchangingapp;
 
-public class PaletteFragment {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class PaletteFragment extends Fragment {
     public PaletteFragment{
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
     }
 }
